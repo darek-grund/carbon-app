ce5cba9dabf0ef0b70e96acafe24999b97c5f577
Dariusz Grund (PGS Software) <dgrund@pgs-soft.com>
2019-06-07 13:53:45 +0200

    Add Carbon Portal Demo

--- src/views/pages/second-page.js
+++ src/views/pages/second-page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Column } from 'carbon-react/lib/components/row';
 import Pill from 'carbon-react/lib/components/pill';
 import Pod from 'carbon-react/lib/components/pod';
+import PortalCarbon from 'carbon-react/lib/components/portal';
 import TextDecorator from '../../components/text-decorator';
 import Portal from '../../components/portal';
 
@@ -17,9 +18,16 @@ const SecondPage = () => {
             </React.Fragment>
           )}
         </TextDecorator>
+
         <Portal target='#portal-target'>
-          <Pod>This content is using portal and should be rendered above navigation menu</Pod>
+          <Pod title='Portal created using ReactDOM.createPortal'>
+            This content is using ReactDOM.createPortal and should be rendered above navigation menu
+          </Pod>
         </Portal>
+
+        <PortalCarbon>
+          <Pod>Portal used by Carbon</Pod>
+        </PortalCarbon>
       </Column>
     </Row>
   );
