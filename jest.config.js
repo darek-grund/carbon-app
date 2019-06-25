55a4ce389ffc1eb200ef3d151cdf9c91a6b64d9c
Darek Grund <dgrund@pgs-soft.com>
2019-06-25 09:06:46 +0200

    Fix Storybook dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "./node_modules/carbon-factory/jest.conf.json",
   testMatch: [
     '**/__spec__.js',
-    '**/*.spec.js'
+    '**/src/**/*.spec.js'
   ],
   setupFiles: [
     './src/__spec_helper__/index.js'
